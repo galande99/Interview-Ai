6cc380f13d1c89094b00ec4d79ce602acbcff6db
Abhilesh Galande <abhileshg1999@gmail.com>
2026-05-12 17:49:32 +0530

    type error

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter} from "react-router-dom"
 import Login from "./Features/auth/pages/Login.jsx"
 import Register from "./Features/auth/pages/Register.jsx"
-import Protected from "./Features/auth/components/protected.jsx"
+import Protected from "./Features/auth/components/Protected.jsx";
 import Home from "./Features/interview/pages/Home.jsx"
 import Interview from "./Features/interview/pages/Interview.jsx"
 
